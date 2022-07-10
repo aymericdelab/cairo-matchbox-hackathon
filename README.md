7dbe91640a3ef7c002ea692d0658e896888fd6b6
= <aymericdelabrousse@gmail.com>
2022-07-10 16:56:21 +0200

    test for reward

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # cairo-matchbox-hackathon
-
-## TODO:
-
-[] find a good representation of the board (2d array)
-[] code the winner function that will check at each move if someone won or not
-[] function that loops around the available positions and finds the next state hash for each of the possible moves
--- tests/test_reward.py
+++ tests/test_reward.py
@@ -5,7 +5,8 @@ import pytest
 from starkware.starknet.testing.starknet import Starknet
 
 # The path to the contract source code.
-CONTRACT_FILE = os.path.join("contracts", "board.cairo")
+CONTRACT_FILE1 = os.path.join("contracts", "board.cairo")
+CONTRACT_FILE2 = os.path.join("contracts", "state_hash_value.cairo")
 
 
 # The testing library uses python's asyncio. So the following
@@ -18,14 +19,30 @@ async def test_increase_balance():
     starknet = await Starknet.empty()
 
     # Deploy the contract.
-    contract = await starknet.deploy(
-        source=CONTRACT_FILE,
+    contract1 = await starknet.deploy(
+        source=CONTRACT_FILE1,
     )
 
-    # Invoke increase_balance() twice.
-    await contract.increase_balance(amount=10).invoke()
-    await contract.increase_balance(amount=20).invoke()
+    # Deploy the contract.
+    contract2 = await starknet.deploy(
+        source=CONTRACT_FILE2,
+    )
 
-    # Check the result of get_balance().
-    execution_info = await contract.get_balance().call()
-    assert execution_info.result == (30,)
+    address = contract2.contract_address
+
+    # state_hash = await contract1.get_state_moves_hash(5, 5, 0).call()
+    # old_value = await contract2.read_state_hash_value(state_hash.result[0]).call()
+    # print(old_value.result[0])
+
+    ## play for both
+    await contract1.write_board(0, 1).invoke()
+    await contract1.write_board(4, 2).invoke()
+    await contract1.write_board(3, 1).invoke()
+    await contract1.write_board(7, 2).invoke()
+    await contract1.write_board(6, 1).invoke()
+
+    await contract1.update_state_hash_value(5+1, 1, address).invoke()
+
+    state_hash = await contract1.get_state_moves_hash(5, 5, 0).call()
+    new_value = await contract2.read_state_hash_value(state_hash.result[0]).call()
+    print(new_value.result[0])
