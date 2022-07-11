5e074f142b5780dae9e730f50ab5ab11baf10ba6
Gregory Edison <gregory.edison1993@gmail.com>
2022-07-11 12:45:26 +0200

    chore: slides

---
+++ view.sh
@@ -0,0 +1,18 @@
+echo "BOARD AT 0"
+nile call board view_board 0
+echo "BOARD AT 1"
+nile call board view_board 1
+echo "BOARD AT 2"
+nile call board view_board 2
+echo "BOARD AT 3"
+nile call board view_board 3
+echo "BOARD AT 4"
+nile call board view_board 4
+echo "BOARD AT 5"
+nile call board view_board 5
+echo "BOARD AT 6"
+nile call board view_board 6
+echo "BOARD AT 7"
+nile call board view_board 7
+echo "BOARD AT 8"
+nile call board view_board 8
